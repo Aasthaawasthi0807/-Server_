34ff4ba0b112b8c61b9e5c1f54b2febbab29986a
Aastha <awasthiaastha1999@gmail.com>
2022-03-05 19:35:19 +0530

    Django admin page customized

--- settings.py
+++ settings.py
@@ -34,6 +34,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+
+    #'admin_interface',
+    #'colorfield',
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
