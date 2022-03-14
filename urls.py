77528363f2ac09eaa1b515d689b23e9d59a323d6
Aastha <awasthiaastha1999@gmail.com>
2022-03-14 10:08:11 +0530

    RBAC Added on user model

--- urls.py
+++ urls.py
@@ -13,16 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import views
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from authentication import views
 #from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 admin.site.site_header = "Leads Management Platform"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('authentication.urls'))
+    path('',include('authentication.urls')),
+    
     
 ]
 
